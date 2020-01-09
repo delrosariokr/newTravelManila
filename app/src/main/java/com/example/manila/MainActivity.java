6cd2f4dea80a74abc5a10803e9391009552be151
Cerceas Bulawan <bulawancs@students.national-u.edu.ph>
2020-01-09 11:27:16 +0800

    NewJomcas

--- app/src/main/java/com/example/manila/MainActivity.java
+++ app/src/main/java/com/example/manila/MainActivity.java
@@ -28,7 +28,10 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter myAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Landmarks> landmarks;
     private DatabaseHelperForUsers mydb;
     private Calendar calendar;
     private SimpleDateFormat dateFormat;
@@ -88,22 +91,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickAdd(View view) {
-        CharSequence[] sequences = new String[10];
         mydb = new DatabaseHelperForUsers(this);
         int UserID = mydb.getIDFromTableUser(user);
         Cursor res = mydb.getAllDataForLandmarks();
-        int i = 0;
-        while (res.moveToNext()) {
-            if (!mydb.CheckIfUser_ListExist(UserID, res.getInt(0))) {
-                sequences[i] = mydb.getLandmarksBaseOnID(res.getInt(0));
-                i++;
+        int count = 0;
+        if (landmarks2.isEmpty()) {
+            while (res.moveToNext()) {
+                if (!mydb.CheckIfUser_ListExist(UserID, res.getInt(0))) {
+                    landmarks2.add(mydb.getLandmarksBaseOnID(res.getInt(0)));
+                }
             }
         }
-        SingleAlgo(sequences,UserID);
-
+        CharSequence[] sequences = new String[landmarks2.size()];
+        for (String temp : landmarks2) {
+            sequences[count] = temp;
+            count++;
+        }
+        SingleAlgo(sequences, UserID);
     }
-
-
 
 
     public void SingleAlgo(final CharSequence[] sequences, final int userID) {
@@ -119,6 +124,7 @@ public class MainActivity extends AppCompatActivity {
                 int landmarkID = mydb.getIDFromTableLandmarks(sequences[which].toString());
                 Boolean CheckInsert = mydb.insertDataInUserVisit(userID, landmarkID, date);
                 if (CheckInsert) {
+                    loadDatabase(userID);
                     Toast.makeText(MainActivity.this, "Successfully Inserted", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
@@ -129,73 +135,62 @@ public class MainActivity extends AppCompatActivity {
         builder.setNeutralButton("Select Multiple Items", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-//                MultipleAlgo(sequences, view, userID);
+                MultipleAlgo(sequences, userID);
             }
         });
         builder.create();
         builder.show();
     }
 
-    //
-//    public void MultipleAlgo(final CharSequence[] sequences, final View view, final int userID) {
-//        final ArrayList selectedItem = new ArrayList();
-//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//        builder.setTitle("Landmarks");
-//        builder.setMultiChoiceItems(sequences, null, new DialogInterface.OnMultiChoiceClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-//                if (isChecked == true) {
-//                    if (!selectedItem.contains(sequences[which])) {
-//                        selectedItem.add(sequences[which]);
-//                        landmarks2.remove(sequences[which]);
-//                    } else {
-//                        selectedItem.remove(sequences[which]);
-//                    }
-//                } else if (isChecked == false) {
-//                    selectedItem.remove(sequences[which]);
-//                }
-//            }
-//        });
-//        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                 dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-//                date = dateFormat.format(calendar.getTime());
-//                for (int i = 0; i < selectedItem.size(); i++) {
-//                    String task = selectedItem.get(i).toString().trim();
-//                    int landmarkID = myDBUSER.getIDFromTableLandmarks(task);
-//                    Boolean CheckInsert = myDBUSER.insertDataInUserVisit(userID, landmarkID, date);
-//                    if (CheckInsert) {
-//                        Cursor res = myDBUSER.getAllDataForList(userID);
-//                        while (res.moveToNext()) {
-//                            String temp = myDBUSER.getLandmarksBaseOnID(res.getInt(1));
-//                            Boolean check = mydb.CheckifLandmarkExist(temp);
-//                            if (!check) {
-//                                Boolean inserted = mydb.insertData(userID,landmarkID, temp);
-//                                if (inserted) {
-//                                    loadDatabase(view,userID);
-//                                    saveData();
-//                                    Toast.makeText(MainActivity.this, "Successfull", Toast.LENGTH_SHORT).show();
-//                                } else {
-//                                    Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        });
-//
-//        builder.setNeutralButton("Back", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                SingleAlgo(sequences, view, userID);
-//            }
-//        });
-//        builder.create();
-//        builder.show();
-//    }
-//
+
+    public void MultipleAlgo(final CharSequence[] sequences, final int userID) {
+        final ArrayList selectedItem = new ArrayList();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Landmarks");
+        builder.setMultiChoiceItems(sequences, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                if (isChecked == true) {
+                    if (!selectedItem.contains(sequences[which])) {
+                        selectedItem.add(sequences[which]);
+                        landmarks2.remove(sequences[which]);
+                    } else {
+                        selectedItem.remove(sequences[which]);
+                    }
+                } else if (isChecked == false) {
+                    selectedItem.remove(sequences[which]);
+                }
+            }
+        });
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+                date = dateFormat.format(calendar.getTime());
+                for (int i = 0; i < selectedItem.size(); i++) {
+                    String task = selectedItem.get(i).toString();
+                    int landmarkID = mydb.getIDFromTableLandmarks(task);
+                    Boolean CheckInsert = mydb.insertDataInUserVisit(userID, landmarkID, date);
+                    if (CheckInsert) {
+                        loadDatabase(userID);
+                        Toast.makeText(MainActivity.this, "Successfully Inserted", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        builder.setNeutralButton("Back", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                SingleAlgo(sequences, userID);
+            }
+        });
+        builder.create();
+        builder.show();
+    }
+
     public void onClickStartActvitiy(View view) {
         View parent = (View) view.getParent();
         TextView text = (TextView) parent.findViewById(R.id.TextViewTitleDisplay);
@@ -204,43 +199,49 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("User", user);
         startActivity(intent);
     }
-//
-//    //    public void onClickDelete(View view) {
-////        System.out.println("HERE AT DELETE");
-////        mydb = new DatabaseHelperForLandmarks(this);
-////        View parent = (View) view.getParent();
-////        TextView taskTextView = (TextView) parent.findViewById(R.id.TextViewTitle);
-////        String task = String.valueOf(taskTextView.getText());
-////        landmarks2.add(task);
-////        int deleterow = mydb.DeleteData(task);
-////        if (deleterow > 0) {
-////            Toast.makeText(this, "Successfully Deleted", Toast.LENGTH_SHORT).show();
-////            loadDatabase(view);
-////            saveData();
-////        } else {
-////            Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
-////        }
-////
-////    }
-////
-//    public void loadDatabase(View view,int userID) {
-//        View parent = (View) ListFragment.passdata.getParent();
-//        recyclerView = parent.findViewById(R.id.List);
+    public void showCustomAlertDialogBox(View view){
+
+    }
+
+
+    //    public void onClickDelete(View view) {
+//        System.out.println("HERE AT DELETE");
 //        mydb = new DatabaseHelperForLandmarks(this);
-//        recyclerView.setHasFixedSize(true);
-//        layoutManager = new LinearLayoutManager(this);
-//        recyclerView.setLayoutManager(layoutManager);
-//        landmarks = new ArrayList<Landmarks>();
-//        Cursor res = mydb.getAllData(userID);
-//        if (res.getCount() == 0) {
-//            landmarks.clear();
+//        View parent = (View) view.getParent();
+//        TextView taskTextView = (TextView) parent.findViewById(R.id.TextViewTitle);
+//        String task = String.valueOf(taskTextView.getText());
+//        landmarks2.add(task);
+//        int deleterow = mydb.DeleteData(task);
+//        if (deleterow > 0) {
+//            Toast.makeText(this, "Successfully Deleted", Toast.LENGTH_SHORT).show();
+//            loadDatabase(view);
+//            saveData();
 //        } else {
-//            while (res.moveToNext()) {
-//                landmarks.add(new Landmarks(res.getString(2), "Add Details", res.getString(2)));
-//            }
+//            Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
 //        }
 //
-//        myAdapter = new LandmarksAdapter(this, landmarks);
-//        recyclerView.setAdapter(myAdapter);
 //    }
+//
+
+    public void loadDatabase(int userID) {
+        View parent = (View) ListFragment.passdata.getParent();
+        recyclerView = parent.findViewById(R.id.List);
+        mydb = new DatabaseHelperForUsers(this);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        landmarks = new ArrayList<Landmarks>();
+        Cursor res = mydb.getAllDataForList(userID);
+        if (res.getCount() == 0) {
+            landmarks.clear();
+        } else {
+            while (res.moveToNext()) {
+                String temp = mydb.getLandmarksBaseOnID(res.getInt(1));
+                landmarks.add(new Landmarks(temp, "Add Details", temp));
+            }
+        }
+
+        myAdapter = new LandmarksAdapter(this, landmarks);
+        recyclerView.setAdapter(myAdapter);
+    }
 }
